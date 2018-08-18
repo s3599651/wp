f7f7d78e2bbe648d81d16c79f84eed7dc5f88e76
Chamila Rendage <s3599651@student.rmit.edu.au>
2018-08-18 17:18:32 +1000

    created product page and added a product

---
+++ a2/products.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang='en'>
+  <head>
+    <meta charset="utf-8">
+    <title>Assignment 2</title>
+
+    <!-- Keep wireframe.css for debugging, add your css to style.css -->
+    <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
+    <link id='stylecss' type="text/css" rel="stylesheet" href="css/style.css">
+    <script src='../wireframe.js'></script>
+  </head>
+
+  <body>
+
+    <header>
+
+      <div>Put company logo and name here</div>
+
+    <nav id = "menu">
+      <ul>
+        <li> <a class = "active" href = "index.php">Home</a></li>
+        <li> <a href = "">Products</a></li>
+        <li> <a href = "login.php">Login</a></li>
+      </ul>
+      <!-- <div>Put website navigation links here</div> -->
+    </nav>
+  </header>
+
+<!-- article means content that is unrelated to the page-->
+    <main id = "products">
+      <article >
+    <h1> Products </h1>
+    <div id = "t-shirt">
+      <p><img src = 'media/onepunch_shirt.png' alt = 'One-punch man t-shrt'>
+      </p>
+    </div>
+      </article>
+    </main>
+
+    <footer>
+      <div>&copy;<script>
+        document.write(new Date().getFullYear());
+        </script> Chamila Rendage(s), s3599651(s)</div>
+      <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
+      <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
+    </footer>
+
+  </body>
+</html>
