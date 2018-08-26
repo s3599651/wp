0f63253661ab40543c14b5246c933c072a6cc6a5
Chamila Rendage <s3599651@student.rmit.edu.au>
2018-08-26 17:00:44 +1000

    added divs to email and password labels

--- a2/login.php
+++ a2/login.php
@@ -29,8 +29,10 @@
       <article id= "loginform">
         <form class = "logform" id= "login" method = "post" action = "https://titan.csit.rmit.edu.au/~e54061/wp/processing.php">
           <h2>User Login</h2>
-          <label for="Email"> Email address: </label> <input type = "email" name= "email" id ="email" value="" placeholder= "Email Address" required><br>
-          <label for = "Pass">Password:</lablel> <input type = "password" id name = "password" placeholder = "required"><br>
+          <div class = "emailholder"<label for="Email"> Email address: </label></div>
+          <input type = "email" name= "email" id ="email" value="" placeholder= "Email Address" required><br>
+          <div class = "passwordholder"><label for = "Pass">Password:</lablel></div>
+          <input type = "password" id name = "password" placeholder = "required"><br>
           <input class = "loginsubmit" type = "submit" value = "Submit">
 
         </form>
