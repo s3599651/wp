6cd610b3adc8f2b03b0c9de1f2d51c734b6cbad0
s3599651 <s3599651@student.rmit.edu.au>
2018-09-02 18:56:20 +1000

    making the image as a link

--- a2/products.php
+++ a2/products.php
@@ -33,10 +33,11 @@
   <div class = "row">
     <div class = "productbox">
 
-      <a href = "index.php"><img id = "t-shirt" src = '../../media/onepunch_shirt.jpg' alt = 'One-punch man t-shrt' ></a>
+      <img id = "t-shirt" src = '../../media/onepunch_shirt.jpg' alt = 'One-punch man t-shrt' <a href = "index.php"></a>>
+      <p> One punch man T-shirt</p>
+      <p> $20.00 </p>
     </div>
-    <p> One punch man T-shirt</p>
-    <p> $20.00 </p>
+
     <div class = "productbox">
       <img id = "jumper" src = '../../media/anime jumper.jpg' alt = 'printed jumper'>
     </div>
