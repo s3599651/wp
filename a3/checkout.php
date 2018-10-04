d9d476bed8b62495c5dcc0aba98113d4a3e30c97
s3599651 <s3599651@student.rmit.edu.au>
2018-10-04 19:52:44 +1000

    added the form

--- a3/checkout.php
+++ a3/checkout.php
@@ -21,8 +21,69 @@
 <div id = "background">
     <main>
       <article class = 'Welcome'>
-        
+        <!-- <div class = "row">
+          <div class = "col-75">
+            <div class = "container"> -->
+              <form action = "">
+                <div class = "row">
+                  <div class = "col-50">
+                    <h3>Billing Address</h3>
+                    <label for ="fname"><i class = "fa-user"></i>Full Name</label>
+                    <input type = "text" id = "fname" name = "firstname" placeholder = "Name">
+                    <label for = "email"><i class = "fa-email"></i>Email</label>
+                    <input type = "text" id = "email" name = "email" placeholder = "john@example.com">
+                    <label for = "adr"><i class = "fa-address"></i>Address</label>
+                    <input type = "text" id ="adr" name = "address">
+                    <label for = "city"><i class = "fa-institution"></i>City</label>
+                    <input type = "text" id = "city" name = "city" placeholder = "Melbourne">
 
+                    <div class = "row">
+                      <div class = "col-50">
+                        <label for = "state">State</label>
+                        <input type = "text" id = "state" name = "state" placeholder = "VIC">
+                      </div>
+                      <div class  = "col-50">
+                        <label for = "Postcode">Postcode</label>
+                        <input type = "text" id = "postcode" name = "postcode" placeholder = "1234">
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class = "col-50">
+                    <h3>Payment</h3>
+                    <label for = "fname"> Accepted Cards</label>
+                    <div class = "icon-container">
+                      <i class = "fa-visa"></i>
+                      <i class = "fa-mastercard"></i>
+                    </div>
+
+                    <label for = "cname">Name on Card</label>
+                    <input type = "text" id = "cname" name = "cardname">
+                    <label for = "ccnum"> Credit card number</label>
+                    <input type = "text" id = "ccnum" name = "cardnumber" placeholder = "XXXX-XXXX-XXXX-XXXX">
+                    <label for = "expmonth">Exp Month</label>
+                    <input type = "text" id = "expmonth" name = "expmonth">
+
+                    <div class = "row">
+                      <div class = "col-50">
+                        <label for = "expyear">Exp Year</label>
+                        <input type = "text" id = "expyear" name = "expyear">
+                      </div>
+
+                      <div class = "col-50">
+                        <label for = "cvv">CVV</label>
+                        <input type = "text" id = "cvv" name = "cvv">
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+
+                <label>
+                  <input type = "checkbox" checked = "checked" name = "sameadr"> Shipping address same as billing </label>
+                  <input type = "submit" value = "Continue to checkout" class = "btn">
+
+                </form>
 
     </article>
   </main>
